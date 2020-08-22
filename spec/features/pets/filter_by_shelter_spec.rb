3589e0b924c3a73e79ee40e2d337095a1777c707
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 14:51:38 -0700

    Update test for Pet filter_by_shelter feature

--- spec/features/pets/filter_by_shelter_spec.rb
+++ spec/features/pets/filter_by_shelter_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe 'the filter by shelter page' do
   it "shows each pet that can be adopted from the shelter" do
-    shelter = Shelter.create!(name: 'Dog Shelter', address: '1st St.', city: 'Bakersfield', state: 'CA', zip: 93303)
+    shelter1 = Shelter.create!(name: 'Dog Shelter', address: '1st St.', city: 'Bakersfield', state: 'CA', zip: 93303)
+    pet1 = Pet.create!(image: 'img001.png', name: 'Olaf', age: 3, sex: 'Male', shelter: shelter1)
+    pet2 = Pet.create!(image: 'img002.png',name: 'Kitty', age: 7, sex: 'Female', shelter: shelter1)
 
-    pet1 = Pet.create!(image: 'img001.png', name: 'Olaf', age: 3, sex: 'Male', shelter: shelter)
-    pet2 = Pet.create!(image: 'img002.png',name: 'Kitty', age: 7, sex: 'Female', shelter: shelter)
+    shelter2 = Shelter.create!(name: 'Cat Shelter', address: '2nd St.', city: 'Los Angeles', state: 'CA', zip: 93303)
+    pet3 = Pet.create!(image: 'img001.png', name: 'Lukas', age: 10, sex: 'Male', shelter: shelter2)
+    pet2 = Pet.create!(image: 'img002.png',name: 'Kitty', age: 7, sex: 'Female', shelter: shelter2)
 
-    visit "/shelters/#{shelter.id}/pets"
+    visit "/shelters/#{shelter1.id}/pets"
 
     save_and_open_page
 
@@ -22,5 +25,8 @@ describe 'the filter by shelter page' do
     expect(page).to have_content(pet2.age)
     expect(page).to have_content(pet2.sex)
     expect(page).to have_content(pet2.shelter.name)
+
+    expect(page).not_to have_content(pet3.name)
+    expect(page).not_to have_content(pet4.name)
   end
 end
