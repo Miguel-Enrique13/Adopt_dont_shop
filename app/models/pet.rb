5b5ed257ebb899b4de789ac0cacbfe14b850bc13
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 12:24:28 -0700

    Create Pet Model

---
+++ app/models/pet.rb
@@ -0,0 +1,8 @@
+class Pet < ApplicationRecord
+  belongs_to :shelter
+  
+  validates_presence_of :image
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :sex
+end
