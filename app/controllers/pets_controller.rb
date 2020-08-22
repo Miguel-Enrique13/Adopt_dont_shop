96f0cf831576659073682c04431c3d81919de63f
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 14:50:50 -0700

    Add filter_by_shelter action to PetsController

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,10 @@
 class PetsController < ApplicationController
   def index
+    @pets = Pet.all
+  end
 
+  def filter_by_shelter
+    shelter = Shelter.find(params[:shelter_id])
+    @pets = shelter.pets
   end
 end
