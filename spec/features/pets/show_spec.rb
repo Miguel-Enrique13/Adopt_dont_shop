b611138c7107613ab0a2a8f1cd30e009ef10ecf2
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 15:09:21 -0700

    Create test for Pet show feature

---
+++ spec/features/pets/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'the pet show page' do
+  it 'shows the pet with that id and its info' do
+    shelter = Shelter.create!(name: 'Cat Shelter', address: '2nd St.', city: 'Los Angeles', state: 'CA', zip: 93303)
+    pet = Pet.create!(image: 'img001.png', name: 'Lukas', age: 10, sex: 'Male', shelter: shelter)
+
+    visit "/pets/#{pet.id}"
+
+    expect(page).to have_content(pet.name)
+    expect(page).to have_content(pet.age)
+    expect(page).to have_content(pet.sex)
+    expect(page).to have_content(pet.shelter.name)
+  end
+end
