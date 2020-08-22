a9015cb8972fd36481154573388021a9671ac338
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 14:40:13 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+  get '/shelters/:shelter_id/pets', to: 'pets#filter_by_shelter'
 end
