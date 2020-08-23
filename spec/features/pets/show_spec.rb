51a69bfdb23804e10f77ee4da3b9a515a8be6c6d
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 17:09:26 -0700

    Update Pet show feature to include adoption_status

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'the pet show page' do
   it 'shows the pet with that id and its info' do
     shelter = Shelter.create!(name: 'Cat Shelter', address: '2nd St.', city: 'Los Angeles', state: 'CA', zip: 93303)
-    pet = Pet.create!(image: 'img001.png', name: 'Lukas', age: 10, sex: 'Male', shelter: shelter, description: "I'm a good boy")
+    pet = Pet.create!(image: 'img001.png', name: 'Lukas', age: 10, sex: 'Male', shelter: shelter, description: "I'm a good boy", adoption_status: 'Adoptable')
 
     visit "/pets/#{pet.id}"
 
@@ -14,5 +14,6 @@ describe 'the pet show page' do
     expect(page).to have_content(pet.sex)
     expect(page).to have_content(pet.description)
     expect(page).to have_content(pet.shelter.name)
+    expect(page).to have_content(pet.adoption_status)
   end
 end
