972320b783d7543c4c57af78e780be0ce26b89d1
Angela Guardia <angelaguardia20@gmail.com>
2020-08-22 18:15:06 -0700

    Create Pet edit and update feature

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,6 +22,16 @@ class PetsController < ApplicationController
     redirect_to "/shelters/#{params[:shelter_id]}/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def update
+    pet = Pet.find(params[:pet_id])
+    pet.update(pet_params)
+    redirect_to "/pets/#{pet.id}"
+  end
+
   private
   def pet_params
     params.permit(:image, :name, :age, :description, :sex, :shelter_id)
