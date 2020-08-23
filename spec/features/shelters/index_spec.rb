64da41e581d6271d8edee1f74b90f68c786daa12
Angela Guardia <angelaguardia20@gmail.com>
2020-08-23 11:37:05 -0700

    Update Shelter Index test to test for update and delete links

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -10,4 +10,26 @@ describe 'shelters index page', type: :feature do
         expect(page).to have_content(shelter_1.name)
         expect(page).to have_content(shelter_2.name)
     end
+
+    it 'can edit shelters from the index page' do
+      shelter_1 = Shelter.create!(name: 'First Shelter', address: '1st St.', city: 'Bakersfield', state: 'CA', zip: 93303)
+
+      visit "/shelters"
+
+      click_on "Update"
+
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+    end
+
+    it 'can delete shelters from the index page' do
+      shelter_1 = Shelter.create!(name: 'First Shelter', address: '1st St.', city: 'Bakersfield', state: 'CA', zip: 93303)
+
+      visit "/shelters"
+
+      click_on "Delete"
+
+      expect(current_path).to eq("/shelters")
+
+      expect(page).not_to have_content("#{shelter_1.name}")
+    end
 end
