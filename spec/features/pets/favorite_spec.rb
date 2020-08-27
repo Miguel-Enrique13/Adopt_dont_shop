595a587005acc554f8bbc13c13df45f11bbfc477
Miguel Guardia <guardia@ualberta.ca>
2020-08-26 20:25:48 -0700

    ADD favorite button on Navbar

--- spec/features/pets/favorite_spec.rb
+++ spec/features/pets/favorite_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'I visit the Pet show page' do
+describe 'I open web page' do
   before :each do
     @shelter1 = Shelter.create!(name: 'Dog Shelter', address: '1st St.', city: 'Bakersfield', state: 'CA', zip: 93303)
     @pet1 = Pet.create!(image: 'dog1.jpg', name: 'Olaf', age: 3, sex: 'Male', shelter: @shelter1)
@@ -8,22 +8,23 @@ describe 'I visit the Pet show page' do
     @review1 = Review.create!(title: "Great Experience!", rating: 4, content: "This shelter is Amazing", shelter: @shelter1)
     @review2 = Review.create!(title: "AMAZING!!!!!!!!!!", rating: 5, content: "This shelter is amazing look at the pup I got!!!", picture: "dog1.jpg", shelter: @shelter1)
   end
+  describe "I visit the pet show page" do
+    it 'I click on Favorite Pet, I see the counter on the nav bar go up, I see a flash message
+    pop up saying "Pet added to Favorites" and I am taken back to pet show page' do
 
-  it 'I click on Favorite Pet, I see the counter on the nav bar go up, I see a flash message
-      pop up saying "Pet added to Favorites" and I am taken back to pet show page' do
+    visit "/pets/#{@pet1.id}"
 
-      visit "/pets/#{@pet1.id}"
+    expect(page).to have_link("Favorite Me!")
 
-      expect(page).to have_link("Favorite Me!")
+    click_on 'Favorite Me!'
 
-      click_on 'Favorite Me!'
+    expect(page).to have_content("Pet has been added to favorites")
 
-      expect(page).to have_content("Pet has been added to favorites")
+    within('#NavBar') do
+      expect(page).to have_content(1)
+    end
 
-      within('#NavBar') do
-        expect(page).to have_content(1)
-      end
-
-      expect(current_path).to eq("/pets/#{@pet1.id}")
+    expect(current_path).to eq("/pets/#{@pet1.id}")
+    end
   end
 end
